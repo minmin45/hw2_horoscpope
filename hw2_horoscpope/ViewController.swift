8fc2f6db171715a7977c7d3076ef3c89784677e6
vv <vv@vvs-Mac.local>
2019-04-17 13:05:07 +0800

    Initial Commit

---
+++ hw2_horoscpope/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  hw2_horoscpope
+//
+//  Created by vv on 2019/4/17.
+//  Copyright © 2019 vv. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ hw2_horoscpopeTests/hw2_horoscpopeTests.swift
@@ -0,0 +1,34 @@
+//
+//  hw2_horoscpopeTests.swift
+//  hw2_horoscpopeTests
+//
+//  Created by vv on 2019/4/17.
+//  Copyright © 2019 vv. All rights reserved.
+//
+
+import XCTest
+@testable import hw2_horoscpope
+
+class hw2_horoscpopeTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
